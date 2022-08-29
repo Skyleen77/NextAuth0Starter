4e05833b530aa4b3f2f41aa65ce9743dc110a23b
Skyleen77 <elsk8@outlook.fr>
2022-08-29 10:44:50 +0200

    update starter

--- components/Menu.js
+++ components/Menu.js
@@ -2,9 +2,8 @@ import Profile from './Profile';
 
 export default function Menu() {
   return (
-    <div>
+    <>
       <a href="/api/auth/login">Login</a> <a href="/api/auth/logout">Logout</a>
-      <Profile />
-    </div>
+    </>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
+import '../styles/globals.css';
+
 import { UserProvider } from '@auth0/nextjs-auth0';
 import Menu from '../components/Menu';
+import Profile from '../components/Profile';
 
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
       <Menu />
+      <Profile />
       <Component {...pageProps} />
     </UserProvider>
   );
